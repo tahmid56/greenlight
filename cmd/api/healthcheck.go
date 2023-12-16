728d6921543803e4859dbcf34c9ad5fec00aefde
Tahmid Akter <tahmidakter56@gmail.com>
2023-12-16 22:55:24 +0600

    enveloping response

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -7,13 +7,19 @@ import (
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request){
 
-	data := map[string]string {
-		"status": "available",
-		"environment": app.config.env,
-		"version": version,
+	// data := map[string]string {
+	// 	"status": "available",
+	// 	"environment": app.config.env,
+	// 	"version": version,
+	// }
+	env := envelope{
+		"status": "status",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version": version,
+		},
 	}
-
-	err := app.writeJSON(w, http.StatusOK, data, nil)
+	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
 		app.logger.Error(err.Error())
 		http.Error(w, "The server encountered an error and couldn't process your request", http.StatusInternalServerError)
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/tahmid56/greenlight/internal/data.go"
 )
 
 func (app *application) createMovieHanlder(w http.ResponseWriter, r *http.Request){
@@ -16,6 +19,17 @@ func (app *application) getMovieHandler(w http.ResponseWriter, r *http.Request){
 		http.NotFound(w, r)
 		return
 	}
-
-	fmt.Fprintf(w, "show the details of movie of id: %v", id)
+	movie := data.Movie {
+		ID: id,
+		CreatedAt: time.Now(),
+		Title: "Casablanca",
+		Runtime: 102,
+		Genres: []string{"drama", "romance", "war"},
+		Version: 1,
+	}
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server envountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
